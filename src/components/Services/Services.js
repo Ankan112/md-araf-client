1f145893eede12f236cee098c736ea5fb77f8c4d
Ankan112 <ankanhalder112@gmail.com>
2022-11-08 13:59:04 +0600

    services card added

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -2,8 +2,49 @@ import React from 'react';
 
 const Services = () => {
     return (
-        <div>
-            <h1>this is servidew</h1>
+        <div className='w-10/12 m-auto'>
+            <h1 className="text-3xl font-semibold text-center my-8">MY SPECIALTIES</h1>
+            <div className='grid lg:grid-cols-3 gap-4 mb-8'>
+                <div className="card bg-base-100 shadow-2xl">
+                    <figure className="px-10 pt-10">
+                        <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card  bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                        <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card  bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                        <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="flex justify-center items-center">
+                <button className="btn btn-outline text-center mb-8">See More</button>
+            </div>
         </div>
     );
 };
