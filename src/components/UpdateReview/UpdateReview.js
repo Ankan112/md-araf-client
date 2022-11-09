ec3d12bd47f9c27ad9da7259f399a752c3603376
Ankan112 <ankanhalder112@gmail.com>
2022-11-10 00:14:43 +0600

    delete review implement

---
+++ src/components/UpdateReview/UpdateReview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpdateReview = () => {
+    return (
+        <div>
+            <h1>update revirew page </h1>
+        </div>
+    );
+};
+
+export default UpdateReview;
