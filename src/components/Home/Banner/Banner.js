93d7fc9fd994e91925065480c6fd594d6e9c20f0
Ankan112 <ankanhalder112@gmail.com>
2022-11-10 14:59:22 +0600

    banner title update

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,15 +1,13 @@
 import React from 'react';
-// import banner from '../../../img/banner.jpg'
 import './Banner.css'
 
 const Banner = () => {
     return (
         <div className='bannerImg '>
-            {/* <img src={banner} alt="" /> */}
             <div className='lg:w-10/12 text-center font-bold lg:text-left pt-48 lg:pl-28'>
                 <div>
                     <h1 className="text-7xl text-white">Hello, <br />I'm Md Araf.</h1>
-                    <h6 className="text-lg text-white">PHOTOGRAPHER, TRAVELER</h6>
+                    <h6 className="text-lg text-white py-6">PHOTOGRAPHER, TRAVELER</h6>
                 </div>
             </div>
         </div>
