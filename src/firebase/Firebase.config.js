4e385c7693c15adaff59f680e4f221094084d263
Ankan112 <ankanhalder112@gmail.com>
2022-11-10 14:39:19 +0600

    don env file added

--- src/firebase/Firebase.config.js
+++ src/firebase/Firebase.config.js
@@ -5,30 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    // apiKey: process.env.REACT_APP_apiKey,
-    // authDomain: process.env.REACT_APP_authDomain,
-    // projectId: process.env.REACT_APP_projectId,
-    // storageBucket: process.env.REACT_APP_storageBucket,
-    // messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    // appId: process.env.REACT_APP_appId,
-    apiKey: "AIzaSyCGipbKXO0UnfhvvKs4d_0LeU7U_4hPTUk",
-    authDomain: "md-araf.firebaseapp.com",
-    projectId: "md-araf",
-    storageBucket: "md-araf.appspot.com",
-    messagingSenderId: "370666955385",
-    appId: "1:370666955385:web:3f2fc3e81908a1a19c2aa8"
-};
-const newitem = {
     apiKey: process.env.REACT_APP_apiKey,
     authDomain: process.env.REACT_APP_authDomain,
     projectId: process.env.REACT_APP_projectId,
     storageBucket: process.env.REACT_APP_storageBucket,
     messagingSenderId: process.env.REACT_APP_messagingSenderId,
     appId: process.env.REACT_APP_appId,
-}
-// Initialize Firebase
-// console.log(firebaseConfig);
-console.log(newitem)
+};
 const app = initializeApp(firebaseConfig);
 
 export default app;
