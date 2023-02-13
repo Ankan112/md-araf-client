1b70e625d8269152e2901dd978cf5dcc249a6b11
Ankan112 <ankanhalder112@gmail.com>
2023-02-14 01:00:50 +0600

    blog page finish

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -3,26 +3,52 @@ import { useState } from 'react';
 import useTitle from '../../hooks/useTitle';
 import Modal from '../Modal/Modal';
 import SingleBlog from './SingleBlog';
+import image1 from '../../img/b1.webp'
+import image2 from '../../img/b2.jpg'
+import image3 from '../../img/b3.jpg'
+import image4 from '../../img/b4.jpg'
+import image5 from '../../img/b5.jpg'
+import image6 from '../../img/b6.jpg'
 
 const Blog = () => {
     useTitle('Blog')
     const [modalInfo, setModalInfo] = useState({});
-    console.log(modalInfo)
     const blogs = [
         {
             id: 1,
-            title: "Example Object",
-            description: "This is an example object with an id, title, and description properties."
+            image: image1,
+            title: "Why should we hire photographer?",
+            description: "Skill and expertise: Professional photographers have the technical expertise, artistic vision, and experience to create high-quality images that capture the moment and tell a story. They have a deep understanding of lighting, composition, and other technical aspects of photography that can be difficult for amateur photographers to master.Professional photographers have access to high-end equipment that can produce stunning images. This equipment can make a big difference in the quality of the final product. Hiring a professional photographer allows you to preserve special moments and memories in a way that will last a lifetime. You can look back on the photos for years to come and relive those moments. Hiring a professional photographer saves you time and effort. You don't have to worry about taking the photos yourself, editing them, or worrying about the final result. You can simply relax and enjoy the moment, knowing that the photographer will handle everything."
         },
         {
             id: 2,
-            title: "Title two object",
-            description: "This is an example object with an id, title, and description properties."
+            image: image2,
+            title: "10 tips for taking better photos with your smartphone",
+            description: "This blog post can provide tips for people who want to take better photos using their smartphone. Topics can include using natural light, adjusting the exposure and focus, using editing apps to enhance the images, and framing the shots effectively."
         },
         {
             id: 3,
-            title: "Hello world",
-            description: "This is an example object with an id, title, and description properties."
+            image: image3,
+            title: "The importance of lighting in photography",
+            description: "Light is one of the most crucial elements in photography, as it has the power to make or break a photo. Good lighting can enhance the mood, add depth and dimension, and create a sense of atmosphere. On the other hand, poor lighting can cause harsh shadows, dull colors, and a flat, unappealing image. Understanding the basics of light, such as its direction, quality, and color, can help photographers make informed decisions about how to use it to their advantage."
+        },
+        {
+            id: 4,
+            image: image4,
+            title: "How to compose a photo: The rule of thirds and other techniques",
+            description: " Composition is the arrangement of elements in a photo, and it can greatly impact the overall look and feel of an image. One popular technique is the rule of thirds, which involves imagining a grid of nine squares over the image, with key elements aligned along the grid lines or at the intersections. This creates a balanced and aesthetically pleasing composition. Other techniques include using leading lines, symmetry, and patterns to guide the eye, as well as playing with negative space to add interest."
+        },
+        {
+            id: 5,
+            image: image5,
+            title: "Landscape photography: Tips for capturing stunning outdoor scenes",
+            description: "Landscape photography is a genre that focuses on capturing the beauty of the natural world, whether it be a sweeping vista, a towering mountain, or a quiet stream. To take stunning landscape photos, photographers should consider the time of day, weather conditions, and lighting. They should also be mindful of foreground elements, as these can add depth and context to the scene. Finally, they should experiment with different angles, perspectives, and compositions to find the one that best captures the essence of the landscape."
+        },
+        {
+            id: 6,
+            image: image6,
+            title: "The evolution of photography: From film to digital and beyond",
+            description: "Photography has come a long way since its invention in the early 19th century. Initially, photographs were captured on film, which required careful processing and printing to produce a final image. With the advent of digital photography, the process became much simpler and more accessible, as images could be captured, stored, and edited using digital cameras and computers. Today, photography continues to evolve with the introduction of new technologies such as artificial intelligence, virtual reality, and augmented reality, offering endless possibilities for capturing and sharing the world around us."
         },
     ]
     return (
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,26 +1,14 @@
 import React from 'react';
 
 const Modal = ({ modalInfo }) => {
-    // console.log(props.blog.description)
     return (
         <div>
-            {/* The button to open modal */}
-
-
-            {/* Put this part before </body> tag */}
-            {/* < input type="checkbox" id="my-modal-4" className="modal-toggle" />
-            <label htmlFor="my-modal-4" className="modal cursor-pointer">
-                <label className="modal-box relative" htmlFor="">
-                    <h3 className="text-lg font-bold"> hi {modalInfo.title}</h3>
-                    <p className="py-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias porro modi eos illo, dolorum nostrum.</p>
-                </label>
-            </label> */}
             <input type="checkbox" id="my-modal-4" className="modal-toggle" />
             <div className="modal">
                 <div className="modal-box relative">
                     <label htmlFor="my-modal-4" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 className="text-lg font-bold">Congratulations random</h3>
-                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet enim aperiam cumque nesciunt vero minima suscipit recusandae inventore eligendi, consequuntur voluptatum dolorum omnis corrupti sapiente veniam dolore! Atque provident illum ex corrupti earum, modi ipsum, ipsa consectetur sequi pariatur sit, laborum reiciendis. Quis consequuntur neque aut, animi doloremque ipsa quibusdam!</p>
+                    <h3 className="text-lg font-bold">{modalInfo.title}</h3>
+                    <p className="py-4">{modalInfo.description}</p>
                 </div>
             </div>
         </div>
